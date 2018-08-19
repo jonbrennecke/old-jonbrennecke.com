a725dd4f7afe4844b0370f8b1eb93fc0d0dc1b8b
Jon Brennecke <jpbrennecke@gmail.com>
2018-08-18 17:02:24 -0700

    header and hero ui - continued

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classnames from 'classnames';
 
 import Logo from '../logo/Logo';
+import MacArtwork from '../artwork/mac/MacArtwork';
 import ContentMaxWidth from '../layout/content-max-width/ContentMaxWidth';
 
 // $FlowFixMe
@@ -26,8 +27,13 @@ export default function Header({
       <ContentMaxWidth className="logo-container">
         <Logo/>
         <div className="hero">
-          <h1>Proven Expertise in Mobile Apps</h1>
-          <p>Hi, I’m Jon. I create apps, websites and services for startups of all shapes and sizes. Let’s talk!</p>
+          <div className="hero-left-side">
+            <h1>Proven Expertise in Mobile Apps</h1>
+            <p>Hi, I’m Jon. I create apps, websites and services for startups of all shapes and sizes. Let’s talk!</p>
+          </div>
+          <div className="hero-right-side">
+            <MacArtwork/>
+          </div>
         </div>
       </ContentMaxWidth>
     </header>
